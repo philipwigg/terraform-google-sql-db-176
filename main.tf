4d6678ac0ad7e549c962a76ccdc440b25eb9fbdd
Philip Wigg <philip.wigg@gfk.com>
2021-01-05 13:25:12 +0000

    Initial commit

---
+++ main.tf
@@ -0,0 +1,13 @@
+module "vpc" {
+  source  = "terraform-google-modules/network/google"
+  project_id = var.project_id
+  network_name = "test-vpc"
+  auto_create_subnetworks = true
+  subnets = []
+}
+
+module "private-service-access" {
+  source  = "GoogleCloudPlatform/sql-db/google//modules/private_service_access"
+  project_id = var.project_id
+  vpc_network = "module.vpc.network_name
+}"
---
+++ terraform.tfvars
@@ -0,0 +1,1 @@
+project_id = "MY-PROJECT-ID"
